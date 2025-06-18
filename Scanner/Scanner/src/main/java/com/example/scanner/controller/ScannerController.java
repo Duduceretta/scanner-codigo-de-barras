9f4692d29c77b0b1241e4863ea4ec31bf112e041
Eduardo Ceretta <duduceretta@gmail.com>
2025-06-17 23:47:08 -0300

    Refactor Coding

---
+++ Scanner/Scanner/src/main/java/com/example/scanner/controller/ScannerController.java
@@ -0,0 +1,14 @@
+package com.example.scanner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ScannerController {
+
+    @GetMapping("/")
+    public String index() {
+        return "scanner/index";
+    }
+
+}
