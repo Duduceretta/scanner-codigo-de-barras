ca954c705cc8a7e641c7c899ba2a3a514e288847
Eduardo Ceretta <duduceretta@gmail.com>
2025-08-30 14:03:43 -0300

    refactor: Cleaning workflow

--- Scanner/Scanner/src/main/java/com/example/scanner/exception/GlobalExceptionHandler.java
+++ Scanner/Scanner/src/main/java/com/example/scanner/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.scanner.exception;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
--- Scanner/Scanner/src/main/java/com/example/scanner/model/Porteiro.java
+++ Scanner/Scanner/src/main/java/com/example/scanner/model/Porteiro.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Porteiro {
 
     @Id
@@ -24,35 +28,4 @@ public class Porteiro {
         this.senha = senha;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
